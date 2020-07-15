2078ccec8239fc8e4777d253a2babcf69a268179
net-pixel <masataka.for.ofua@gmail.com>
2020-07-16 01:06:22 +0900

    ActionText文章検索

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def search
     @search = Article.ransack(params[:q])
     @articles = @search.result.includes(:category)
+
   end
   def search_result
     @search = Article.ransack(params[:q])
--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,15 @@ class Article < ApplicationRecord
 
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 25 }
+
+  scope :body, -> (search_param = nil) {
+    return if search_param.blank?
+    joins("INNER JOIN action_text_rich_texts ON action_text_rich_texts.record_id = articles.id AND action_text_rich_texts.record_type = 'Article'")
+    .where("action_text_rich_texts.body LIKE ? OR articles.title LIKE ? ", "%#{search_param}%", "%#{search_param}%")
+  }
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i(body)
+  end
+  
 end
