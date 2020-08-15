d39933de857e1fa1de7edc1091aa389788e50c39
net-pixel <masataka.for.ofua@gmail.com>
2020-08-15 14:51:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "articles#index"
 
-  resources :articles do
-    collection do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
-    member do
-      get 'get_category_children', defaults: { format: 'json' }
-      get 'get_category_grandchildren', defaults: { format: 'json' }
-    end
-  end
+  resources :articles
 
   get 'about', to: 'pages#about'
   get 'search', to: 'pages#search'
