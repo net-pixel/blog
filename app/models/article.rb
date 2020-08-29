321e827df8c48a97e96bb12d44302bcdb36cfa71
net-pixel <masataka.for.ofua@gmail.com>
2020-08-29 09:27:40 +0900

    ShowOrderbyNewArticle

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,7 @@ class Article < ApplicationRecord
   has_one_attached :banner
   has_rich_text :body
   acts_as_taggable
-
+  default_scope -> { order(created_at: :desc) }
 
   validates :title, length: { minimum: 5 }
   validates :body, length: { minimum: 25 }
